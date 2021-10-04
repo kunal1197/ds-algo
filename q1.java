9bf5603d3afb2deadc2b71ca95b7f28cee41aa01
Kunal Mehrotra <kunal.007ma@gmail.com>
2021-10-04 02:18:37 -0400

    Create q1.java
    java codes

---
+++ q1.java
@@ -0,0 +1,20 @@
+class space1
+{
+void main()
+{
+int i;int j;int k;int sp=4;
+for(i=1;i<=5;i++)
+{
+for(k=1;k<=sp;k++)
+{
+System.out.print(" ");
+}
+for(j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println();
+sp--;
+}
+}
+}
